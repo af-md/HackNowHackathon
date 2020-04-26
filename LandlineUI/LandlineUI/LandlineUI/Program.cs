d6058cf7540553d96a6a6acc1e2b3aa1153b532f
AdamMKD <adammkd@hotmail.com>
2020-04-26 18:26:15 +0100

    landline ui

---
+++ LandlineUI/LandlineUI/LandlineUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Linq;
+
+namespace LandlineUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dbClient = new MongoClient("mongodb+srv://adam2:ZiVHBr3zjNlVFbWk@landline-e216g.mongodb.net/test?retryWrites=true&w=majority");
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+
+            var database = dbClient.GetDatabase("Landline");
+            var collection = database.GetCollection<BsonDocument>("test");
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
